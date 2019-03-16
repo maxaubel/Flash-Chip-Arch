3a8c500f946a96e82fd3f8981c8c3e5968528eeb
Richard Schembri <drahcir.sanguin@gmail.com>
2019-03-16 18:03:11 +0900

    First commit

--- Flash.sh
+++ Flash.sh
@@ -1,8 +1,58 @@
 #!/bin/bash
 
+setup_enviroment() { \
+	echo -e "\n Setting up environment"
+	sudo pacman -Syy
+	while IFS=, read -r program comment; do
+		n=$((n+1))
+		if [ $n -gt 1 ]; then
+		 echo -e "\n Installing [$program]"
+		 echo -e "\n [$program] $comment \n"
+		 sudo pacman --noconfirm --needed -S --color=auto "$program"
+			if ! pacman -Qs $program > /dev/null ; then
+			  echo "Failed to install [$program] exiting"
+			  exit 0
+			fi
+		fi
+	done < programs.csv ;}
 
+setup_usergroups(){
+	echo -e "\n Adding current user to dialout group"
+	sudo usermod -a -G dialout $(logname)
 
+	echo -e "\n Adding current user to plugdev group"
+	sudo usermod -a -G plugdev $(logname)
+}
 
+setup_allwinner_udevrules(){
+	echo -e "\n Adding udev rule for Allwinner devices"
+	echo -e 'SUBSYSTEM=="usb", ATTRS{idVendor}=="1f3a", ATTRS{idProduct}=="efe8", GROUP="plugdev", MODE="0660" SYMLINK+="usb-chip"
+	SUBSYSTEM=="usb", ATTRS{idVendor}=="18d1", ATTRS{idProduct}=="1010", GROUP="plugdev", MODE="0660" SYMLINK+="usb-chip-fastboot"
+	SUBSYSTEM=="usb", ATTRS{idVendor}=="1f3a", ATTRS{idProduct}=="1010", GROUP="plugdev", MODE="0660" SYMLINK+="usb-chip-fastboot"
+	SUBSYSTEM=="usb", ATTRS{idVendor}=="067b", ATTRS{idProduct}=="2303", GROUP="plugdev", MODE="0660" SYMLINK+="usb-serial-adapter"
+	' | sudo tee /etc/udev/rules.d/99-allwinner.rules
+	sudo udevadm control --reload-rules
+}
+
+install_chip_tools(){
+	echo -e "\n Installing CHIP-tools"
+	if [ -d CHIP-tools ]; then
+	 cd CHIP-tools 
+	 git pull 
+	 elif [ ! -d CHIP-tools ]; then
+	 git clone https://github.com/Project-chip-crumbs/CHIP-tools.git
+	 cd  CHIP-tools 
+	 FEL='sudo sunxi-fel' FASTBOOT='sudo fastboot' SNIB=false ./chip-update-firmware.sh -$flavour
+	fi
+}
+
+flash_chip(){
+	 cd  CHIP-tools
+	 FEL='sudo sunxi-fel' FASTBOOT='sudo fastboot' SNIB=false ./chip-update-firmware.sh -"$1"
+}
+
+  echo ""
+  echo ""
   echo "   #  #  #"
   echo "  #########"
   echo "###       ###"
@@ -12,68 +62,55 @@
   echo "###       ###"
   echo "  ########"
   echo "   #  #  #"
+  echo ""
+  echo ""
 
 
-echo " Welcome to the C.H.I.P Flasher Tool " 
+echo " Welcome to the C.H.I.P Flashing Tool "
+echo ""
+echo " Instuctions"
+echo " ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ "
+echo " 1. Remove the C.H.I.P from its case (in case you have a Pocket "
+echo "    C.H.I.P)."
+echo " 2. Connect the FEL and a GND(Ground) pin of the C.H.I.P with a "
+echo "    wire or paperclip."
+echo " 3. Connect the C.H.I.P micro USB port to a USB port of your "
+echo "    ArchLinux machine."
+echo " 5. Make sure that the LEDs beside the micro USB port of the "
+echo "    C.H.I.P have turned on"
+echo ""
+echo " Choose your flavour "
+echo " ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ "
+echo " 'p' for the Pocket CHIP image "
+echo " 's' for the headless server image "
+echo " 'g' for the desktop image "
+echo " 'b' for the Buildroot image "
+echo " 'x' to exit "
+echo ""
+echo " IMPORTANT NOTICE "
+echo " ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ "
+echo " If your CHIP is suffering from POWER ISSUES add an 'n' to your "
+echo " choice of flavour. "
+echo " Example: 'gn' for the No-Limit desktop Image "
+echo ""
+echo " Additional options "
+echo " ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ "
+echo " 'f' for Force Clean "
+echo ""
+echo " ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ "
+echo " Then press [enter] to start the process"
+echo " [enter] to start the process"
 
-echo " Please enter your wanted flavour " 
-echo " ++++++++++++++++++++++++++++++++++++++++++ "
-echo " enter p for pocketchip Image "
-echo " enter s for the headless Server Image "
-echo " enter g for the Desktop Image "
-echo " enter b for the Buildroot Image " 
-echo " ++++++++++++++++++++++++++++++++++++++++++ "
-echo " IMPORTANT INFO "
-echo " If u suffer from Power Problems add a n "
-echo " to your choice of flavour "
-echo " Example: gn for the No-Limit Desktop Image "
-echo " ++++++++++++++++++++++++++++++++++++++++++ "
-echo " Other options " 
-echo " ++++++++++++++++++++++++++++++++++++++++++ "
-echo " enter f for Force Clean " 
-echo " ++++++++++++++++++++++++++++++++++++++++++ "
-echo " Then press enter please " 
+read flavour 
 
-read flavour
-
-
-
-echo -e "\n Setting up environment"
-sudo apt-get -y update
-sudo apt-get install curl -y
-sudo apt-get install u-boot-tools -y
-sudo apt-get -y install \
- git \
- fastboot \
- sunxi-tools \
- u-boot-tools \
- adb \
- u-boot-tools \
- android-tools-fastboot \
-
-echo -e "\n Adding current user to dialout group"
-sudo usermod -a -G dialout $(logname)
-
-echo -e "\n Adding current user to plugdev group"
-sudo usermod -a -G plugdev $(logname)
-
-
-echo -e "\n Adding udev rule for Allwinner device"
-echo -e 'SUBSYSTEM=="usb", ATTRS{idVendor}=="1f3a", ATTRS{idProduct}=="efe8", GROUP="plugdev", MODE="0660" SYMLINK+="usb-chip"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="18d1", ATTRS{idProduct}=="1010", GROUP="plugdev", MODE="0660" SYMLINK+="usb-chip-fastboot"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="1f3a", ATTRS{idProduct}=="1010", GROUP="plugdev", MODE="0660" SYMLINK+="usb-chip-fastboot"
-SUBSYSTEM=="usb", ATTRS{idVendor}=="067b", ATTRS{idProduct}=="2303", GROUP="plugdev", MODE="0660" SYMLINK+="usb-serial-adapter"
-' | sudo tee /etc/udev/rules.d/99-allwinner.rules
-sudo udevadm control --reload-rules
-
-echo -e "\n Installing CHIP-tools"
-if [ -d CHIP-tools ]; then
- cd CHIP-tools 
- git pull 
- FEL='sudo sunxi-fel' FASTBOOT='sudo fastboot' SNIB=false ./chip-update-firmware.sh -$flavour
- elif [ ! -d CHIP-tools ]; then
- git clone https://github.com/Project-chip-crumbs/CHIP-tools.git
- cd  CHIP-tools 
- FEL='sudo sunxi-fel' FASTBOOT='sudo fastboot' SNIB=false ./chip-update-firmware.sh -$flavour
+if [ -z "$flavour" ]
+then
+	echo "No input. Exiting"
+	exit 0
 fi
+
+setup_enviroment
+setup_usergroups
+setup_allwinner_udevrules
+flash_chip "$flavour"
 
--- README.md
+++ README.md
@@ -1,18 +1,22 @@
-# Flash-CHIP
-Ready to use Flash environment for the C.H.I.P Single Board Computer
-
-Simplyfies the Flashing Process for the C.H.I.P and PocketC.H.I.P Computer. 
+# Flash-CHIP from Archlinux
+A script that simplyfies the flashing process for the C.H.I.P and PocketC.H.I.P Computer.
+Designed to run on ArchLinux.
 
 ## Instructions
 1. Remove the C.H.I.P from its case (in case you have a Pocket C.H.I.P).
-2. Connect the FEL and a GROUND pin of the C.H.I.P (for example, with a paperclip).
-3. Connect the C.H.I.P its micro USB port to a USB port of your Linux machine.
-4. In the Linux machine:
-    - run ` git clone https://github.com/Thore-Krug/Flash-CHIP.git` to clone this repository.
+2. Connect the FEL and a GND(Ground) pin of the C.H.I.P with a wire or paperclip.
+3. Connect the C.H.I.P micro USB port to a USB port of your ArchLinux machine.
+4. In the terminal of your ArchLinux machine run the following commands:
+5. Make sure that the LEDs beside the micro USB port of the C.H.I.P have turned on
+    - ` git clone https://github.com/richardschembri/Flash-CHIP` to clone this repository.
     - `cd` into the location where you stored this repository.
     - run `sudo chmod +x Flash.sh`
     - run `./Flash.sh`
-    - Select the version you want to install.
-    - Wait until the installation finishes.
+    - Select the image you wish to flash on your CHIP.
+    - You might be prompted for your sudo password.
+    - The script should run automatically from this point on.
     - Enjoy!
   
+## References
+* C.H.I.P tools taken from https://archive.org/details/C.h.i.p.FlashCollection
+* The script downloads the C.H.I.P images from http://chip.jfpossibilities.com/chip/
